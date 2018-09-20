b1bbd9d06e10b4a58758775497d7c024c00aa9ed
Cnels1986 <CNels1986@gmail.com>
2018-09-20 10:50:18 -0400

    csv file loaded and array created with county name and population as key and value pairs

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 <?php
+
   function loadCsv($path) {
       $csvFile = file($path);
       $csv = array_map('str_getcsv', $csvFile);
@@ -6,12 +9,26 @@
         $a = array_combine($csv[0], $a);
       });
       array_shift($csv); # remove column header
+      array_shift($csv);
       return $csv;
   }
-  // est72017sex0_age999
-  // GEO.display-label
 
-  // $censusData = loadCsv("")
-  echo "Test";
+  function getPopulation($data) {
+    $population = [];
+    $a = 0;
+    forEach ($data as $dataPoint) {
+      $name = $dataPoint['GEO.display-label'];
+      $pop= intval($dataPoint['est72017sex0_age999']);
+      $population[$a] = [
+        'county'=> $name,
+        'pop' => $pop];
+      $a++;
+    }
+    return $population;
+  }
 
+  $censusData = loadCsv('http://localhost/dataAnalysis/pacensus.csv');
+  // var_dump($censusData);
+  $test = getPopulation($censusData);
+  var_dump($test);
 ?>
